ff564c4f86692e1d4a387976146154e2e7798a76
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-10-09 16:24:28 +0300

    Finish 3rd assignment: Greetings to Name and Surname

--- test/ViewControllers/ControlsViewController.swift
+++ test/ViewControllers/ControlsViewController.swift
@@ -9,45 +9,58 @@
 import UIKit
 
 class ControlsViewController: UIViewController {
-
+    
     // MARK: - Outlets:
-
+    
     @IBOutlet var screenView: UIView!
     @IBOutlet var colorChangeSwitch: UISwitch!
     
     
-    
-    
-    
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var surnameTextField: UITextField!
+    @IBOutlet var greetingsTextLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        screenView.backgroundColor = .white
+        colorChangeSwitch.isOn = true
+        
+        nameTextField.text = ""
+        surnameTextField.text = ""
+        greetingsTextLabel.text = ""
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-        // MARK: - Actions:
+    // MARK: - Actions:
     @IBAction func printHelloWorldButtonTapped(_ sender: Any) {
         print("Hello World!!!")
     }
     
     @IBAction func checkSwitchBtnTapped(_ sender: UIButton) {
-
-        
+        if colorChangeSwitch.isOn {
+            screenView.backgroundColor = .red
+        } else {
+            screenView.backgroundColor = .white
+        }
     }
     
-    
-    
-    
+    @IBAction func sayGreetingBtnTapped(_ sender: UIButton) {
+        
+        var nameString = ""
+        var surnameString = ""
+        
+        if let name = nameTextField.text{
+            nameString = name
+        }
+        
+        if let surname = surnameTextField.text{
+            surnameString = surname
+        }
+        
+        if nameString + surnameString != "" {
+            greetingsTextLabel.text = "Hello \(nameString) \(surnameString) !!!"
+        } else {
+            greetingsTextLabel.text = "Please enter Name and Surname in fields above!"
+        }
+    }
 }
