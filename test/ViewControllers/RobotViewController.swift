959221f31e13d811a1f5013361dd97cb1be2b05a
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-10-09 12:27:57 +0300

    Load project done until now

---
+++ test/ViewControllers/RobotViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RobotViewController.swift
+//  test
+//
+//  Created by Office-iMac on 2020-10-01.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import UIKit
+
+class RobotViewController: UIViewController {
+
+    
+    @IBOutlet var leftHand: UIView!
+    @IBOutlet var leftLeg: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    
+    @IBAction func segmentedControlTapped(_ sender: UISegmentedControl) {
+        
+    }
+    
+}
---
+++ test/ViewControllers/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Office-iMac on 2020-10-01.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
