666ff42fe5bc53adda2b9a700b33a26942ace16c
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-10-09 22:26:12 +0300

    Finish Lecture4 Task1 Fill table with images

---
+++ test/ViewControllers/ReceiptsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ReceiptsViewController.swift
+//  test
+//
+//  Created by Anton on 2020-10-09.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    
+    @IBOutlet var receiptsTableView: UITableView!
+    
+    var receiptsImages = [UIImage(named: "white"), UIImage(named: "yellow"), UIImage(named: "blue")]
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadTable()
+    }
+    
+    
+
+    
+
+    
+    // MARK: - TableSetup:
+    func loadTable() {
+        receiptsTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return receiptsImages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let receiptCell = receiptsTableView.dequeueReusableCell(withIdentifier: "ReceiptTableCell", for: indexPath) as! ReceiptTableCell
+        
+        let ImageInCurrentRow = receiptsImages[indexPath.row]
+        
+        receiptCell.receiptImage.image = ImageInCurrentRow
+        
+        return receiptCell
+    }
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ test/Views/ReceiptTableCell.swift
@@ -0,0 +1,27 @@
+//
+//  ReceiptTableCell.swift
+//  test
+//
+//  Created by Anton on 2020-10-09.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptTableCell: UITableViewCell {
+
+    @IBOutlet var receiptImage: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
