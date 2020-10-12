86a0853270160325941ac23ad7b0c50c81c17078
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-10-12 21:18:06 +0300

    Update result showing logic

--- test/ViewControllers/ShowGameResultViewController.swift
+++ test/ViewControllers/ShowGameResultViewController.swift
@@ -4,18 +4,30 @@ class ShowGameResultViewController: UIViewController {
 
     @IBOutlet weak var resultLabel: UILabel!
     
+    let minimalScore = 5
     var gameResult = Int()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        resultLabel.text = String(gameResult)
+        
+        setLabelText()
         
     }
     
+    func gameIsWon() -> Bool{
+        if gameResult >= minimalScore {
+            return true
+        } else {
+            return false
+        }
+    }
 
-    
-    
-    
-
+    func setLabelText(){
+        
+        if gameIsWon() {
+            resultLabel.text = "Congratulations!!! Game is WON with result \(String(gameResult))"
+        } else {
+            resultLabel.text = "Ohh!!! Game is Losed. Minimal score is: \(minimalScore) and your result is only: \(String(gameResult))"
+        }
+    }
 }
