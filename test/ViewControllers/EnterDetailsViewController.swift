fd692f068dafd53ea1f3c74ec258d82a2254e695
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-10-17 14:06:46 +0300

    start Lecture 6

---
+++ test/ViewControllers/EnterDetailsViewController.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+struct User {
+    var name: String
+    var surname: String
+    
+    var gender: String?
+    var coutnry: String?
+    var city: String?
+    var street: String?
+    var bio: String?
+}
+
+
+
+class EnterDetailsViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+//    ShowUserDetailsVCSegue
+    
+}
